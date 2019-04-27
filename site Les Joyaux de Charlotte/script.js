6bc8ebbbc8130740a1bbe22c232daa0989b84519
Benjul76 <benjul76@hotmail.fr>
2019-04-27 16:31:07 +0200

    création

---
+++ site Les Joyaux de Charlotte/script.js
@@ -0,0 +1,13 @@
+﻿(function($) {
+
+  $('.feedly-list-item > a').each(function(){
+    $(this).on('click', function(){      
+      if ( $(this).hasClass('on') ) {
+      $(this).removeClass('on').addClass('off');
+      } else {
+      $(this).removeClass('off').addClass('on');
+      } 
+      return false;      
+    });
+  });
+})
